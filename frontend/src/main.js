a20022e10cf04407aa673698befc520d65c4cbb3
Akrobate <fedorov.artiom@gmail.com>
2020-01-10 14:06:43 +0100

    added material design, sass loader, scaffold application with materia design

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+// import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+// import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
 import App from './App'
 import router from './router'
+
+Vue.use(VueMaterial)
+
+// Vue.use(MdContent)
+// Vue.use(MdTabs)
+// Vue.use(MdButton)
 
 Vue.config.productionTip = false
 
