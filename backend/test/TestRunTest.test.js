805c5ac7fd9ecc38fdb013888e28ca8ee2db743c
Akrobate <fedorov.artiom@gmail.com>
2020-01-14 23:23:30 +0100

    Added a dummy test, and started travis configuration file

---
+++ backend/test/TestRunTest.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Dummy test just to test if travis integration works correctly
+ */
+
+describe('TestRunTest', () => {
+    it('Step should pass always properly', (done) => {
+        done();
+    });
+});
