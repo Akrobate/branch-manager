e373fbd91af29e92fa12590019c3b33b0c33ef40
Akrobate <fedorov.artiom@gmail.com>
2020-01-23 14:10:09 +0100

    Added inclusions and directories to the project

--- backend/src/repositories/index.js
+++ backend/src/repositories/index.js
@@ -4,12 +4,16 @@ const {
     FileSystemRepository,
 } = require('./FileSystemRepository');
 
-
 const {
     GitCredentialRepository,
 } = require('./GitCredentialRepository');
 
+const {
+    ProjectRepository,
+} = require('./ProjectRepository');
+
 module.exports = {
     FileSystemRepository,
     GitCredentialRepository,
+    ProjectRepository,
 };
