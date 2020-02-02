0ddae0378c27d41adde27c78eaeaade7f2869573
Akrobate <fedorov.artiom@gmail.com>
2020-02-02 15:30:58 +0100

    Added new project route

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -13,5 +13,6 @@ const router = new Router();
 const credential_controller = CredentialController.getInstance();
 
 router.get('/credentials', (request, response) => credential_controller.getCredentials(request, response));
+router.get('/projects', (request, response) => project_controller.getAllProjects(request, response));
 
 module.exports = router;
