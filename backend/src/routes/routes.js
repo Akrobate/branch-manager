f007a623202d642508ebf0ccea7edb88c1e71a12
Akrobate <fedorov.artiom@gmail.com>
2020-02-03 13:38:02 +0100

    Fixed route for projects

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -8,9 +8,14 @@ const {
     CredentialController,
 } = require('../controllers');
 
+const {
+    ProjectController,
+} = require('../controllers');
+
 const router = new Router();
 
 const credential_controller = CredentialController.getInstance();
+const project_controller = ProjectController.getInstance();
 
 router.get('/credentials', (request, response) => credential_controller.getCredentials(request, response));
 router.get('/projects', (request, response) => project_controller.getAllProjects(request, response));
