67f815e31aef702465914f3e011ace456b62cee4
Akrobate <fedorov.artiom@gmail.com>
2020-05-11 23:33:33 +0200

    [FRONT] Added routes for new pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import HomePage from '@/components/pages/HomePage'
+import CredentialsPage from '@/components/pages/CredentialsPage'
+import ProjectsPage from '@/components/pages/ProjectsPage'
 
 Vue.use(Router)
 
@@ -11,6 +13,16 @@ export default new Router({
             path: '/',
             name: 'HomePage',
             component: HomePage,
+        },
+        {
+            path: '/credentials',
+            name: 'CredentialsPage',
+            component: CredentialsPage,
+        },
+        {
+            path: '/projects',
+            name: 'ProjectsPage',
+            component: ProjectsPage,
         },
     ],
 })
