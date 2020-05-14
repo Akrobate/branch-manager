b1c7abb5c855f7d68bb0e7d76d12f6a96f9bbc4d
Akrobate <fedorov.artiom@gmail.com>
2020-05-14 19:16:17 +0200

    Added mocks to the acceptance project test to use seeds data

--- backend/test/acceptance/projects.controller.unit.test.js
+++ backend/test/acceptance/projects.controller.unit.test.js
@@ -1,4 +1,9 @@
+'use strict';
+
 const supertest = require('supertest');
+const {
+    mock,
+} = require('sinon');
 
 const {
     expect,
@@ -8,7 +13,37 @@ const {
     server,
 } = require('../../src/server');
 
+const {
+    FileSystemRepository,
+} = require('../../src/repositories');
+
+const seeds_data_dir = `${__dirname}/../seeds/data/`;
+
+const mocks = {};
+
 describe('Projects controller', () => {
+
+    beforeEach(() => {
+        mocks.file_system_repository = mock(FileSystemRepository.getInstance());
+    });
+
+    afterEach(() => {
+        mocks.file_system_repository.restore();
+    });
+
+    it('Should be able to list all projects', (done) => {
+        mocks.file_system_repository.expects('getDataDir').returns(seeds_data_dir);
+        supertest(server)
+            .get('/projects')
+            .expect(200)
+            .end((error, result) => {
+                expect(error).to.equal(null);
+                expect(result).to.have.property('body');
+                // console.log(result.body);
+                done();
+            });
+    });
+
 
     it('Should be able to list all projects', (done) => {
         supertest(server)
@@ -17,9 +52,11 @@ describe('Projects controller', () => {
             .end((error, result) => {
                 expect(error).to.equal(null);
                 expect(result).to.have.property('body');
+                // console.log(result.body);
                 done();
             });
     });
+
 
     it.skip('Should be able get a project by project id', (done) => {
         const project_id = 'test_project';
