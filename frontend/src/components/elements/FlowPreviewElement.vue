bef997ec4cfc31503de573882db657e43adcdd5b
Akrobate <fedorov.artiom@gmail.com>
2020-05-21 19:58:47 +0200

    Added flow preview reusable component block

---
+++ frontend/src/components/elements/FlowPreviewElement.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+
+import {
+    project_repository,
+} from '@/repositories/ProjectRepository'
+
+export default {
+    props: {
+        project_id: 'string',
+    },
+    name: 'RepositoriesPage',
+    components: {
+    },
+    mounted() {
+        project_repository
+            .getProject(this.project_id)
+            .then((data) => {
+                this.flow_data = data.branch_flow
+            })
+    },
+    data() {
+        return {
+            data_list: [
+
+            ],
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
