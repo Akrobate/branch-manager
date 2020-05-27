6ec83f596a3211ad65f76fe56ee346adc3d2bf51
Akrobate <fedorov.artiom@gmail.com>
2020-05-27 23:24:39 +0200

    Added tests for git utilitary method

--- backend/test/services/GitService.unit.test.js
+++ backend/test/services/GitService.unit.test.js
@@ -10,15 +10,9 @@ const {
     GitService,
 } = require('../../src/services');
 
+const git_service = GitService.getInstance();
 
-describe('GitService unit test', () => {
-
-    let git_service = null;
-
-    before(() => {
-        git_service = GitService
-            .getInstance();
-    });
+describe('GitService Commands unit test', () => {
 
     it('clone', () => {
         const url = v4();
@@ -38,6 +32,28 @@ describe('GitService unit test', () => {
 
     it('fetchAllPrune', () => {
         expect(git_service.fetchAllPruneCommand()).to.equal('git fetch --all --prune');
+    });
+
+});
+
+describe('GitService Utilitary methods', () => {
+
+    it('getRepositoryDomainFromUrl - github.com', () => {
+        expect(git_service
+            .getRepositoryDomainFromUrl('https://github.com/Akrobate/branch-manager.git')
+        ).to.equal('github.com');
+    });
+
+    it('getRepositoryDomainFromUrl - gitlab.com', () => {
+        expect(git_service
+            .getRepositoryDomainFromUrl('https://gitlab.com/Akrobate/artiom-fedorov-resume.git')
+        ).to.equal('gitlab.com');
+    });
+
+    it('getRepositoryDomainFromUrl - framagit.org', () => {
+        expect(git_service
+            .getRepositoryDomainFromUrl('https://framagit.org/Akrobate/jekyll-openscad-theme.git')
+        ).to.equal('framagit.org');
     });
 
 });
