29832b7606b2e2f946d9e7552abfd3a7a02e9fe6
Akrobate <fedorov.artiom@gmail.com>
2020-06-01 22:16:05 +0200

    WIP ProcessService, added main loop

--- backend/src/services/ProcessService.js
+++ backend/src/services/ProcessService.js
@@ -13,12 +13,15 @@ class ProcessService {
         return ProcessService.instance;
     }
 
+
     /**
      * @returns {ProcessService}
      */
     constructor() {
         this.is_processing = false;
+        this.job_queue_list = [];
     }
+
 
     /**
      * @param {String} url
@@ -26,9 +29,51 @@ class ProcessService {
      */
     process() {
         this.is_processing = true;
-        // code to process goes here
 
+        // code to process goes here
         this.is_processing = false;
+    }
+
+
+    /**
+     * @returns {Pormise}
+     */
+    executeJobsInQueue() {
+        const job_to_execute = this.job_queue_list.shift();
+        if (this.queueIsEmpty()) {
+            return Promise.resolve();
+        }
+        return new Promise(
+            (resolve, reject) => job_to_execute
+                .execute()
+                .then(() => this.executeJobsInQueue())
+                .then(resolve)
+                .catch(reject)
+        );
+    }
+
+
+    /**
+     * @param {JobAbstract} job
+     * @returns {Void}
+     */
+    addJob(job) {
+        this.job_queue_list.push(job);
+    }
+
+
+    /**
+     * @returns {Boolen}
+     */
+    isProcessing() {
+        return this.is_processing;
+    }
+
+    /**
+     * @returns {Boolean}
+     */
+    queueIsEmpty() {
+        return this.job_queue_list.length === 0;
     }
 
 }
