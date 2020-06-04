141266acfe6a050377b0ee84f419f54292b98916
Akrobate <fedorov.artiom@gmail.com>
2020-06-04 21:37:57 +0200

    Added tools to the UpdateProjectJob

--- backend/src/services/jobs/UpdateProjectRepositoriesJob.js
+++ backend/src/services/jobs/UpdateProjectRepositoriesJob.js
@@ -12,15 +12,31 @@ const {
     ProjectService,
 } = require('../ProjectService');
 
+const {
+    CommandLineService,
+} = require('../CommandLineService');
+
+const {
+    GitService,
+} = require('../GitService');
+
 class UpdateProjectRepositoriesJob extends AbstractJob {
 
     /**
      * @param {ProjectService} project_service
+     * @param {CommandLineService} command_line_service
+     * @param {GitService} git_service
      * @returns {UpdateProjectRepositoriesJob}
      */
-    constructor(project_service) {
+    constructor(
+        project_service,
+        command_line_service,
+        git_service
+    ) {
         super();
         this.project_service = project_service;
+        this.command_line_service = command_line_service;
+        this.git_service = git_service;
         this.project_id = null;
     }
 
@@ -31,7 +47,9 @@ class UpdateProjectRepositoriesJob extends AbstractJob {
      */
     static buildJob() {
         return new UpdateProjectRepositoriesJob(
-            ProjectService.getInstance()
+            ProjectService.getInstance(),
+            CommandLineService.getInstance(),
+            GitService.getInstance()
         );
     }
 
