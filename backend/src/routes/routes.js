2b9d044d9d2525304a8c4b35ef1743884da09269
Akrobate <fedorov.artiom@gmail.com>
2020-06-08 21:20:15 +0200

    Added route to get process status

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -12,14 +12,22 @@ const {
     ProjectController,
 } = require('../controllers');
 
+const {
+    ProcessController,
+} = require('../controllers');
+
 const router = new Router();
 
 const credential_controller = CredentialController.getInstance();
 const project_controller = ProjectController.getInstance();
+const process_controller = ProcessController.getInstance();
 
 router.get('/credentials', (request, response) => credential_controller.getCredentials(request, response));
+
 router.get('/projects/:id', (request, response) => project_controller.getProject(request, response));
 router.get('/projects/:id/process/update', (request, response) => project_controller.processProjectUpdateRepository(request, response));
 router.get('/projects', (request, response) => project_controller.getAllProjects(request, response));
+
+router.get('process/status', (request, response) => process_controller.getStatus(request, response));
 
 module.exports = router;
