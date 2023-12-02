b296c74534dc95916cbb9a78bd8c22c7ebe1dfdb
Arkobate <fedorov.artiom@gmail.com>
2023-12-02 22:03:59 +0100

    Testing raw git wtih parent

--- backend/test.js
+++ backend/test.js
@@ -20,4 +20,22 @@ const simpleGit = require('simple-git');
 
     res = await git.checkout('develop');
     console.log(res);
+
+    // Testing //
+    
+    git.raw(['log', '--pretty=format:%H %P %s']).exec((err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        const commits = result.split('\n').map(commit => {
+            const [hash, parents, message] = commit.split(' ');
+            return { hash, parents: parents.split(','), message };
+        });
+
+        console.log(commits);
+    });
 })();
+
+
