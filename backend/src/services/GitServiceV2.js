a6922effecb995b978385bd124cffa0e162e6804
Arkobate <fedorov.artiom@gmail.com>
2023-12-05 18:35:21 +0100

    Preparing GetServiceV2

---
+++ backend/src/services/GitServiceV2.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const public_suffix_list = require('psl');
+
+class GitService {
+
+    /* istanbul ignore next */
+    /**
+     * @return {GitService}
+     */
+    static getInstance() {
+        if (GitService.instance === null) {
+            GitService.instance = new GitService();
+        }
+        return GitService.instance;
+    }
+
+
+    /**
+     * @param {String} url
+     * @return {String}
+     */
+    cloneCommand(url) {
+        return `git clone ${url}`;
+    }
+
+
+    /**
+     * @param {String} branch
+     * @return {String}
+     */
+    checkoutCommand(branch) {
+        return `git checkout ${branch}`;
+    }
+
+
+    /**
+     * @param {String} origin
+     * @param {String} branch
+     * @return {String}
+     */
+    pullCommand(origin, branch) {
+        return `git pull ${origin} ${branch}`;
+    }
+
+
+    /**
+     * @param {String} origin
+     * @param {String} branch
+     * @return {String}
+     */
+    fetchAllPruneCommand() {
+        return 'git fetch --all --prune';
+    }
+
+    /**
+     * @param {String} url
+     * @return {String}
+     */
+    getRepositoryDomainFromUrl(url) {
+        let hostname = '';
+        if (url.indexOf('//') > -1) {
+            // eslint-disable-next-line prefer-destructuring
+            hostname = url.split('/')[2];
+        } else {
+            // eslint-disable-next-line prefer-destructuring
+            hostname = url.split('/')[0];
+        }
+
+        // eslint-disable-next-line prefer-destructuring
+        hostname = hostname.split(':')[0];
+
+        // eslint-disable-next-line prefer-destructuring
+        hostname = hostname.split('?')[0];
+
+        return public_suffix_list.get(hostname);
+    }
+
+}
+
+GitService.instance = null;
+
+module.exports = {
+    GitService,
+};
