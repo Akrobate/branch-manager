18ca4235e220e9ec872163f57b39cfe298b7604d
Arkobate <fedorov.artiom@gmail.com>
2023-12-26 20:02:24 +0100

    Created GitRepository

---
+++ backend/src/repositories/GitRepository.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const simpleGit = require('simple-git');
+
+
+class GitRepository {
+
+    /* istanbul ignore next */
+    /**
+     * @return {GitRepository}
+     */
+    static getInstance() {
+        if (GitRepository.instance === null) {
+            GitRepository.instance = new GitRepository();
+        }
+        return GitRepository.instance;
+    }
+
+
+    /**
+     * @param {*} path_to_git
+     * @param {*} GIT_SSH_COMMAND
+     * @return {object}
+     */
+    buildGitInstance(path_to_git, GIT_SSH_COMMAND) {
+
+        let git = null;
+        if (path_to_git) {
+            git = simpleGit(
+                path_to_git,
+                {
+                    binary: 'git',
+                }
+            );
+        } else {
+            git = simpleGit(
+                {
+                    binary: 'git',
+                }
+            );
+        }
+
+        return git.env('GIT_SSH_COMMAND', GIT_SSH_COMMAND);
+    }
+
+
+    /**
+     * @param {String} ssh_key_file_path
+     * @return {String}
+     */
+    generateSshCommand(ssh_key_file_path) {
+        const GIT_SSH_COMMAND = `ssh -o StrictHostKeyChecking=no -i ${ssh_key_file_path}`;
+        return GIT_SSH_COMMAND;
+    }
+
+
+    /**
+     * @param {String} git_url
+     * @param {String} path_to_git
+     * @param {*} GIT_SSH_COMMAND
+     * @return {String}
+     */
+    async cloneCommand(git_url, path_to_git, GIT_SSH_COMMAND) {
+        const git = this.buildGitInstance(path_to_git, GIT_SSH_COMMAND);
+        const result = await git.clone(git_url, path_to_git);
+        return result;
+    }
+
+
+    /**
+     * @param {String} branch
+     * @param {String} path_to_git
+     * @param {String} GIT_SSH_COMMAND
+     * @return {String}
+     */
+    async checkout(branch, path_to_git, GIT_SSH_COMMAND) {
+        const git = this.buildGitInstance(path_to_git, GIT_SSH_COMMAND);
+        const result = await git.checkout(branch);
+        return result;
+    }
+
+
+    /**
+     * @param {String} path_to_git
+     * @param {String} GIT_SSH_COMMAND
+     * @return {String}
+     */
+    async fetchAllPrune(path_to_git, GIT_SSH_COMMAND) {
+        const git = this.buildGitInstance(path_to_git, GIT_SSH_COMMAND);
+        const result = await git.fetch({
+            '--prune': null,
+            '--all': null,
+        });
+        return result;
+    }
+
+
+    /**
+     * @param {String} path_to_git
+     * @param {String} GIT_SSH_COMMAND
+     * @param {String} remote
+     * @param {String} branch
+     * @return {String}
+     */
+    async pull(path_to_git, GIT_SSH_COMMAND, remote, branch) {
+        const git = this.buildGitInstance(path_to_git, GIT_SSH_COMMAND);
+        const result = await git.pull(remote, branch);
+        return result;
+    }
+
+}
+
+GitRepository.instance = null;
+
+module.exports = {
+    GitRepository,
+};
