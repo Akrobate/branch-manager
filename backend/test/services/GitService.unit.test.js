cf90f86a479a11204516213209b1cda7678d1b5b
Arkobate <fedorov.artiom@gmail.com>
2024-01-21 20:10:00 +0100

    Added useless git tests

--- backend/test/services/GitService.unit.test.js
+++ backend/test/services/GitService.unit.test.js
@@ -3,38 +3,10 @@ const {
 } = require('chai');
 
 const {
-    v4,
-} = require('uuid');
-
-const {
     GitService,
 } = require('../../src/services');
 
 const git_service = GitService.getInstance();
-
-describe.skip('GitService Commands unit test', () => {
-
-    it('clone', () => {
-        const url = v4();
-        expect(git_service.clone(url)).to.equal(`git clone ${url}`);
-    });
-
-    it('checkout', () => {
-        const branch = v4();
-        expect(git_service.checkout(branch)).to.equal(`git checkout ${branch}`);
-    });
-
-    it('pull', () => {
-        const branch = v4();
-        const origin = v4();
-        expect(git_service.pullCommand(origin, branch)).to.equal(`git pull ${origin} ${branch}`);
-    });
-
-    it('fetchAllPrune', () => {
-        expect(git_service.fetchAllPruneCommand()).to.equal('git fetch --all --prune');
-    });
-
-});
 
 describe('GitService Utilitary methods', () => {
 
