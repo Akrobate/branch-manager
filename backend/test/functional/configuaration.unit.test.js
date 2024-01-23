4935af46a39e70bb1be0e6b4297a640b65accc3a
Arkobate <fedorov.artiom@gmail.com>
2024-01-23 16:15:39 +0100

    Enriched test, configuration structure

--- backend/test/functional/configuaration.unit.test.js
+++ backend/test/functional/configuaration.unit.test.js
@@ -15,4 +15,13 @@ describe('Configuration unit test', () => {
     it('Should be able to get Application port', () => {
         expect(configuration.server.port).to.equal(3000);
     });
+
+    it('Check necessary properties', () => {
+        expect(configuration).to.have.property('server');
+        expect(configuration.server).to.have.property('port');
+
+        expect(configuration).to.have.property('storage');
+        expect(configuration.storage).to.have.property('data_file_path');
+    });
+
 });
